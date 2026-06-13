967486c7c4370ee50935197ef23976818b13bdc4
Akash S Pillai <akashpillai40@gmail.com>
2026-06-13 12:48:19 +0530

    feat: setup Redux auth slice, store, Login page

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,50 @@
+
+    
+    
+   import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full border-b border-gray-200 bg-white">
+      <div className="max-w-7xl mx-auto px-8 py-5 flex items-center justify-between">
+        
+        {/* Logo */}
+        <div className="flex items-center gap-3">
+          <div className="w-10 h-10 rounded-xl bg-gradient-to-r from-blue-600 to-purple-600 flex items-center justify-center text-white text-xl">
+            AI
+          </div>
+
+          <h1 className="text-3xl font-bold text-gray-900">
+            InterviewAI
+          </h1>
+        </div>
+
+        {/* Nav Links */}
+        <div className="hidden md:flex items-center gap-12 text-gray-600 font-medium">
+          <a href="#" className="hover:text-blue-600 transition">
+            Features
+          </a>
+
+          <a href="#" className="hover:text-blue-600 transition">
+            How It Works
+          </a>
+
+          <a href="#" className="hover:text-blue-600 transition">
+            Testimonials
+          </a>
+
+          <a href="#" className="hover:text-blue-600 transition">
+            Sign In
+          </a>
+
+          <button className="px-8 py-3 rounded-xl text-white font-semibold bg-gradient-to-r from-blue-600 to-purple-600 hover:scale-105 transition">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+export default Navbar
---
+++ src/store/authSlice.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//login API call async-thunk
+export const loginUser = createAsyncThunk(
+    'auth/loginUser',
+     async(formData) => {
+          const response = await axios.post('http://localhost:5000/api/auth/login', formData)
+          return response.data   //{user, token}
+     }
+)
+//signup API call async-thunk
+export const registerUser = createAsyncThunk(
+    'auth/registerUser',
+    async(formData) => {
+       const response = await axios.post('http://localhost:5000/api/auth/register', formData)
+        return response.data   //{user, token} === action.payload
+    }
+   
+)
+//reducer Name: 'auth'
+//creating slice
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+        token: null,
+        isLoading: false,
+        error: null
+    },
+    reducers: {
+        //sync only logout
+        logout:(state) => {
+            state.user = null
+            state.token = null
+            state.error = null
+        }
+    }, 
+    //Async actions state update with data from API {user, token} and initialStates
+    extraReducers: (builder) => {
+        //Login User
+        builder.addCase(loginUser.pending, (state) => {
+            state.isLoading = true
+            state.error = null
+        })
+        builder.addCase(loginUser.fulfilled, (state, action) => {  
+            state.isLoading = false
+            state.error = null
+            state.user = action.payload.user
+            state.token = action.payload.token
+        })
+        builder.addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false
+            state.error = action.error.message
+        })
+
+        //if register user
+        builder.addCase(registerUser.pending, (state) => {
+            state.isLoading = true
+            state.error = null
+        })
+        builder.addCase(registerUser.fulfilled, (state, action) => { 
+ //if user reg in MongoDB will give {user, token} => response.data === action.payload
+            state.isLoading = false
+            state.error = null
+            state.user = action.payload.user
+            state.token = action.payload.token
+        })
+        builder.addCase(registerUser.rejected, (state, action) => {
+            state.error = action.error.message
+            state.isLoading = false
+        })
+    }
+})
+ export const { logout } = authSlice.actions
+ export default authSlice.reducer
