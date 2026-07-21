ad2607974316915205b0a6be6a33fa903a897d2f
Akash S Pillai <akashpillai40@gmail.com>
2026-07-21 12:59:56 +0530

    Configure CORS origin using environment variable fallback

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -1,5 +1,8 @@
 import axios, { create } from "axios";
 
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
